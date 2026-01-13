93e79b28e5e53b769167eb2f759412f55321f319
HumayunAhmed20 <anikhumayun110@gmail.com>
2026-01-14 01:17:37 +0600

    login html and error

--- PROJECT/project p/MVC/Views/login.php
+++ PROJECT/project p/MVC/Views/login.php
@@ -9,3 +9,78 @@ $error =$auth ->handleLogin();
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/project p\MVC\Views\CSS\login.css">
+
+    <title> 
+
+    Login  </title>
+
+  
+  </head>
+<body>
+
+
+
+<section>
+
+<div class="container">
+<div class="row">
+    
+
+<div>
+    <img src="../../MVC/Images/draw2.webp" alt="image">
+
+</div>
+
+
+<div>
+
+
+<div class="card">
+          <form method="POST">
+       <h2>Login</h2>
+
+      
+      
+      <?php
+            if (!empty($error))
+                 {
+                    echo '<div class="alert">' . $error . '</div>';
+
+                    } ?>
+
+    <label>Email address</label>
+          
+        <input type="email" name="email" placeholder="Enter a valid email" required><br><br>
+
+    <label>Password</label>
+           <input type="password" name="password" placeholder="Enter password" required><br><br>
+
+
+      <div class="check">
+           
+        <input type="checkbox" id="remember" name="remember">
+      
+        <label for="remember">Remember Me</label>
+           </div>
+
+        <button type="submit" name="login">Login</button>
+    
+        <div class="login-text">
+
+        Don't have an account? <a href="/project p/MVC/Views/register.php">Register</a>
+</div>
+
+   
+</form>
+ </div>
+    </div>
+         </div>
+           </div>
+</section>
+
+</body>
+</html>
