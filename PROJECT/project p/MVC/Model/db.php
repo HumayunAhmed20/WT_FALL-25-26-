ec61ccf02df467d61ff603a292133bd6ad4924d3
HumayunAhmed20 <anikhumayun110@gmail.com>
2026-01-15 09:04:13 +0600

    database

--- PROJECT/project p/MVC/Model/db.php
+++ PROJECT/project p/MVC/Model/db.php
@@ -1,2 +1,16 @@
-<html>
-    </html>
+<?php
+class DataBase {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db   = "huma";
+ 
+    public function connect() {
+        $conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        if ($conn->connect_error) {
+            die("Database Connection Failed");
+        }
+        return $conn;
+    }
+    
+}
