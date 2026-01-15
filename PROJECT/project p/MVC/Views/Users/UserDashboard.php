675666b686a1a12bbadc896d5b2dd52b482c5526
HumayunAhmed20 <anikhumayun110@gmail.com>
2026-01-15 11:48:54 +0600

    Add dashboard

---
+++ PROJECT/project p/MVC/Views/Users/UserDashboard.php
@@ -0,0 +1,43 @@
+<?php
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <link rel="stylesheet" href="../../Views/CSS/userdashboard.css">
+   
+     <meta charset="UTF-8">
+    
+     <title>User Dashboard</title>
+</head>
+<body>
+
+<div class="sidebar">
+    
+<h3>User</h3>
+     <a href="UserDashboard.php">Dashboard</a>
+   
+   
+     <a href="UpcomingEvents.php">Upcoming Events</a>
+    <a href="MyProfile.php">My Profile</a>
+    <a href="../../Controller/logout.php">Logout</a>
+</div>
+
+
+<div class="topbar">
+   
+    <h2>Student Dashboard</h2>
+    <p>Welcome, User</p>
+</div>
+
+<div class="main">
+
+    <div class="alert">Welcome to the Sports Event Dashboard</div>
+
+
+    
+    
+
+</body>
+</html>
