4a0265380194f12f9e204ad31dc055754c6d5910
HumayunAhmed20 <anikhumayun110@gmail.com>
2026-01-16 16:42:17 +0600

    volunteer dashboard layout and navigation

---
+++ PROJECT/project p/MVC/Views/Volunteer/RegistrationDashboard.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>
+       Manage Registrations
+       </title>
+<link rel="stylesheet" href="../Views/CSS/1.css">
+<style>
+
+</style>
+</head>
+
+<body>
+
+
+<div class="sidebar">
+    <h3>Volunteer</h3>
+    <a href="/WT_FALL-25-26-/PROJECT/project%20p/MVC/Views/Volunteer/dashboard.php">Dashboard</a>
+    <a href="#">Registrations</a>
+  <a href="/WT_FALL-25-26-/PROJECT/project%20p/MVC/Controller/logout.php">Logout</a>
+</div>
+
+
+
+<div class="top-navbar">
+  <h5>Manage Registrations</h5>
+  <div style="color:#0d6efd;">
+     <div>Welcome, <?= htmlspecialchars($_SESSION['username']) ?></div>
+  </div>
+</div>
+
+
+
+</body>
+</html>
